def4aeadda81a42ffe6d5f78f59f7b6c9fe8d3c4
aditya25122005 <144265921+aditya25122005@users.noreply.github.com>
2024-01-12 22:05:22 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,13 @@
+a=1
+print(a)
+b="aditya"
+a1=9
+print(a+a1)
+c=False
+d= None
+e= complex(8,2)
+print("The type of a is ",type(a))
+print("the type of b is",type(b))
+print("The type of c is",type(c))
+print("The type of d is",type(d))
+print ("type of e is",type(e))
